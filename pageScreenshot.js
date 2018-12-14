fb7da499348877f11a0c4ae88df6a0e46d7e64b3
Loskir <kl@sinker.ru>
2018-12-15 00:27:02 +0300

    изменит время ожидания готовности записи и изначальный viewport

--- pageScreenshot.js
+++ pageScreenshot.js
@@ -4,7 +4,8 @@ module.exports = async id => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(`http://math.loskir.ru/${id}`);
-  await page.waitFor('#math');
+  await page.setViewport({width:10,height:10});
+  await page.waitFor(2000);
   const dimensions = await page.evaluate(() => {
     return {
       width: document.querySelector('#math').scrollWidth,
