3adae2769c9c556213f7a88fade5ec758ae77487
Loskir <kl@sinker.ru>
2018-12-15 00:51:50 +0300

    изменит механизм получения скриншота

--- pageScreenshot.js
+++ pageScreenshot.js
@@ -1,21 +1,21 @@
 const puppeteer = require('puppeteer');
 
 module.exports = async id => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto(`http://math.loskir.ru/${id}`);
-  await page.setViewport({width:10,height:10});
-  await page.waitFor(2000);
-  const dimensions = await page.evaluate(() => {
-    return {
-      width: document.querySelector('#math').scrollWidth,
-      height: document.querySelector('#math').scrollHeight,
-      deviceScaleFactor: window.devicePixelRatio
-    };
-  });
-  console.log(dimensions);
-  await page.setViewport(dimensions);
-  await page.screenshot({path: `./img/${id}.png`});
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(`http://math.loskir.ru/${id}`);
+    await page.setViewport({width:10,height:10});
+    await page.waitFor('#math');
+    const dimensions = await page.evaluate(() => {
+        return {
+            width: document.querySelector('#math').scrollWidth+100,
+            height: document.querySelector('#math').scrollHeight+60,
+            deviceScaleFactor: window.devicePixelRatio
+        };
+    });
+    console.log(dimensions);
+    await page.setViewport(dimensions);
+    await page.screenshot({path: `./${id}.png`});
 
-  await browser.close();
+    await browser.close();
 };
